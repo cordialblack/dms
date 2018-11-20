2d61aea9870b3f55ad304d418d8052700882088d
cordialblack <cordialblack@gmail.com>
2018-11-20 08:20:55 -0600

    first

---
+++ mk_dms.sh
@@ -0,0 +1,14 @@
+## Set up Dockerized Media Server
+
+## determine linux distribution
+
+## identify which services should be set up
+
+## identify and create a root directory for persistent data
+
+## add a unified group for app user membership
+
+## main body - install services
+
+## clean up - set permissions on common directories
+
