5b3ec44c7dfd0976cbf649134e9d36e4acd7d30e
cordialblack <cordialblack@gmail.com>
2018-11-27 05:45:21 -0600

    Using variable to accommodate deb/ubu

--- mk_docker.sh
+++ mk_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ## https://docs.docker.com/install/linux/docker-ce/debian/
+. /etc/os-release
 
 apt-get update
 
@@ -11,10 +12,9 @@ apt-get -y install \
 	gnupg2 \
 	software-properties-common
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-
+curl -fsSL https://download.docker.com/linux/$ID/gpg | sudo apt-key add -
 sudo add-apt-repository \
-	"deb [arch=amd64] https://download.docker.com/linux/debian \
+	"deb [arch=amd64] https://download.docker.com/linux/$ID \
 	$(lsb_release -cs) \
 	stable"
 
